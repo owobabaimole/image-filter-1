1d1fe2ed72f8e2e312c4f332974bea20491257db
owobabaimole <tayorakinyele6@gmail.com>
2023-01-23 04:51:27 +0100

    fixed

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import axios from "axios"; 
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -7,25 +8,41 @@ import Jimp = require("jimp");
 // INPUTS
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
-//    an absolute path to a filtered image locally saved file
+//    an absolute path to a filtered image locally saved file 
+
+
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
-        });
+      axios({
+          method: 'get',
+          url: inputURL,
+          responseType: 'arraybuffer'
+      })
+      .then(async ({data: imageBuffer}) => {
+        const photo = await Jimp.read(imageBuffer);
+        const outpath =
+          "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        await photo
+          .resize(256, 256) // resize
+          .quality(60) // set JPEG quality
+          .greyscale() // set greyscale
+          .write(__dirname + outpath, (img) => {
+            resolve(__dirname + outpath);
+          });
+      })
     } catch (error) {
       reject(error);
     }
   });
 }
+
+
+
+
+
+
+
 
 // deleteLocalFiles
 // helper function to delete files on the local disk
